4d92cf4bbacb132f07ada92812560eb29f632ec6
pujadudhat <pooja.gajera11@gmail.com>
2017-06-08 23:09:15 -0700

    Initial commit

---
+++ app/src/main/java/csc519/itu/edu/csc519_hw4_92537/DetailActivity.java
@@ -0,0 +1,65 @@
+package csc519.itu.edu.csc519_hw4_92537;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ActionProvider;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class DetailActivity extends Activity {
+
+    private ActionProvider shareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        String details = getIntent().getStringExtra("SELECTED_DAY_WEATHER_DETAILS");
+        Log.d(MainActivity.APP_TAG, "DetailActivity Text : " + details);
+
+        TextView textView = (TextView) findViewById(R.id.detail_textview);
+        textView.setText(details);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.detail, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        shareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+        setShareIntent(createShareIntent());
+
+        return true;
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(shareIntent);
+        }
+    }
+
+    /** Returns a share intent */
+    private Intent createShareIntent(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "SUBJECT");
+        intent.putExtra(Intent.EXTRA_TEXT,"Extra Text");
+        return intent;
+    }
+
+}
+
